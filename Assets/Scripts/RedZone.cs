533d0aa6b77cfc2dad59f03191864ce68c5f0703
MD Rifat Hossain Chowdhury <59175725+RS-Rifat@users.noreply.github.com>
2023-05-07 00:50:19 +0600

    Project file

--- Assets/Scripts/RedZone.cs
+++ Assets/Scripts/RedZone.cs
@@ -6,18 +6,8 @@ using UnityEngine.UI;
 
 public class RedZone : MonoBehaviour
 {
-    [SerializeField] private GameObject gameOverUI;
     [SerializeField] private Button restartButton;
 
-    private void Start()
-    {
-        gameOverUI.SetActive(false);
-    }
-
-    /*private void Awake()
-    {
-        restartButton.onClick.AddListener(RestartTheGame);
-    }*/
     private void OnTriggerStay(Collider other)
     {
         Cube cube = other.GetComponent<Cube>();
@@ -26,14 +16,9 @@ public class RedZone : MonoBehaviour
         {
             if(!cube.IsMainCube && cube.CubeRigidbody.velocity.magnitude < 01f)
             {
-                gameOverUI.SetActive(true);
-                restartButton.onClick.AddListener(RestartTheGame);
+                // Load Mane GameOver Scene
+
             }
         }
-    }
-
-    private void RestartTheGame()
-    {
-        gameOverUI.SetActive(false);
     }
 }
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    private void Start()
+    {
+        // if button clicked action here
+    }
+
+    private void Awake()
+    {
+        // if button clicked load main game scene
+    }
+}
